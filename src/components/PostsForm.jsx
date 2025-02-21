189f1c01139ce23919c04132fe7927c9f109c895
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-21 17:23:41 +0100

    correzione .content

--- src/components/PostsForm.jsx
+++ src/components/PostsForm.jsx
@@ -79,8 +79,8 @@ const PostsForm = () => {
                         <li
                             key={post.id}>
                             <h2>{post.title}</h2>
-                            <p className="corsive">Tag: {post.tags.map((tag) => (tag))}</p>
-                            <p>{post.contenuto}</p>
+                            <p className="corsive">Tag: {post.tags}</p>
+                            <p>{post.content}</p>
                             <div className="img-container"><img src={`http://localhost:3000${post.image}`} /></div>
                             {post.available && <p className="status">Pubblicato</p>}
                             <button onClick={() => removePost(post.id)}>Cancella</button>
